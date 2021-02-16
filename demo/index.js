ff73f60c642f51f4e43127b14bedbcb04fc2cf04
lujjjh <lujjjh@gmail.com>
2021-02-16 21:25:39 +0800

    Demo: Support backspace and arrow keys

---
+++ demo/index.js
@@ -0,0 +1,105 @@
+import * as PyJS from "https://cdn.jsdelivr.net/npm/@programmingplus/pyjs@0.0.4";
+
+document.getElementById("loading-indicator").style.display = "none";
+
+const term = new Terminal({
+  fontSize: 14,
+  lineHeight: 1.6,
+  fontFamily:
+    '"Fira Mono", "DejaVu Sans Mono", Menlo, Consolas, "Liberation Mono", Monaco, "Lucida Console", monospace',
+});
+const fitAddon = new FitAddon.FitAddon();
+term.loadAddon(fitAddon);
+term.open(document.getElementById("terminal"));
+fitAddon.fit();
+const observer = new ResizeObserver((mutations) => void fitAddon.fit());
+observer.observe(document.getElementById("terminal"));
+term.write(`# click the "Run" button above\r\n`);
+
+const run = document.getElementById("run");
+run.disabled = false;
+run.addEventListener("click", async () => {
+  term.reset();
+  run.disabled = true;
+  const write = (s) => void term.write(s.replace(/\n/g, "\r\n"));
+  let stdinBuffer = "";
+  let resolveStdinBufferPromise;
+  let stdinBufferPromise;
+  const resetStdinBuffer = () => {
+    stdinBuffer = "";
+    stdinBufferPromise = new Promise((resolve) => void (resolveStdinBufferPromise = resolve));
+  };
+  resetStdinBuffer();
+  let lineBuffer = [];
+  let lineBufferPos = 0;
+  const appendLineBuffer = (s) => {
+    const appendix = [...s];
+    lineBuffer = [...lineBuffer.slice(0, lineBufferPos), ...appendix, ...lineBuffer.slice(lineBufferPos)];
+    lineBufferPos += appendix.length;
+  };
+  const flushSuffix = () => {
+    const appendix = lineBuffer.slice(lineBufferPos);
+    term.write(appendix.join(""));
+    if (appendix.length > 0) {
+      term.write(`\x1b[${appendix.length}D`);
+    }
+  };
+  const onDataEvent = term.onData((data) => {
+    switch (data) {
+      case "\r": // enter
+        term.write("\r\n");
+        stdinBuffer += lineBuffer.join("") + "\n";
+        lineBuffer = [];
+        lineBufferPos = 0;
+        resolveStdinBufferPromise();
+        break;
+      case "\x7f": // backspace
+        if (lineBufferPos > 0) {
+          term.write("\x1b[D\x1b[K");
+          lineBuffer.splice(lineBufferPos - 1, 1);
+          lineBufferPos--;
+          flushSuffix();
+        }
+        break;
+      case "\x1b[A": // arrow up
+      case "\x1b[B": // arrow down
+        break;
+      case "\x1b[D": // arrow left
+        if (lineBufferPos > 0) {
+          lineBufferPos--;
+          term.write(data);
+        }
+        break;
+      case "\x1b[C": // arrow right
+        if (lineBufferPos < lineBuffer.length) {
+          lineBufferPos++;
+          term.write(data);
+        }
+        break;
+      default: {
+        term.write(data);
+        appendLineBuffer(data);
+        term.write("\x1b[K");
+        flushSuffix();
+      }
+    }
+  });
+  try {
+    await PyJS.run(document.getElementById("editor").value, {
+      writeStdout: write,
+      writeStderr: write,
+      readStdin: async () => {
+        term.focus();
+        await stdinBufferPromise;
+        const buffer = stdinBuffer;
+        resetStdinBuffer();
+        return buffer;
+      },
+    });
+    term.write("\r\n\x1b[1;30m[Program terminated]\x1b[0m\r\n");
+  } catch (error) {
+    term.write(`\r\n\x1b[1;31m${error.message}\x1b[0m\r\n`);
+  }
+  onDataEvent.dispose();
+  run.disabled = false;
+});
